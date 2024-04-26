435ac0807a8a9eb99c23bd47276dee53bed8aea0
Chikombe <chikombe.ecy@gmail.com>
2024-04-26 09:34:29 +0000

    2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,11 +1,11 @@
 export default function handleResponseFromAPI(promise) {
-  promise
+  return promise
     .then(() => {
       console.log('Got a response from the API');
       return { status: 200, body: 'success' };
     })
     .catch(() => {
       console.log('Got a response from the API');
-      return new Error();
+      return { status: 200, body: 'error' }; // Return a response for error case
     });
 }
