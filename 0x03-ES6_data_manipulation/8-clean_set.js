070d277b04380646d4577ffb700874c63291f764
Chikombe <chikombe.ecy@gmail.com>
2024-04-30 11:13:51 +0000

    8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,9 @@
 export default function cleanSet(set, startString) {
-  const values = [...set];
-  const filteredValues = values.filter((value) => value.startsWith(startString));
-  return filteredValues.map((value) => value.substring(startString.length)).join('-');
+  if ((set instanceof Set) && (typeof startString === 'string')) {
+    let array = Array.from(set);
+    const len = startString.length;
+    array = array.filter((a) => typeof a === 'string' && a[0] === startString[0] && a.startsWith(startString));
+    return array.map((s) => s.substring(len)).join('-');
+  }
+  return '';
 }
