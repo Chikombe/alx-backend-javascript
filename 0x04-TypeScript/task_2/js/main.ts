1f8065f14c97f612ef3cee21beb2c814e5de375d
Chikombe <chikombe.ecy@gmail.com>
2024-05-01 15:11:04 +0000

    Task 5. Advanced types Part 1

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,64 @@
+// Define DirectorInterface
+interface DirectorInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
+}
+
+// Define TeacherInterface
+interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
+}
+
+// Define Director class implementing DirectorInterface
+class Director implements DirectorInterface {
+  workFromHome(): string {
+    return "Working from home";
+  }
+
+  getCoffeeBreak(): string {
+    return "Getting a coffee break";
+  }
+
+  workDirectorTasks(): string {
+    return "Getting to director tasks";
+  }
+}
+
+// Define Teacher class implementing TeacherInterface
+class Teacher implements TeacherInterface {
+  workFromHome(): string {
+    return "Cannot work from home";
+  }
+
+  getCoffeeBreak(): string {
+    return "Cannot have a break";
+  }
+
+  workTeacherTasks(): string {
+    return "Getting to work";
+  }
+}
+
+// Define createEmployee function
+function createEmployee(salary: number | string): Director | Teacher {
+  if (typeof salary === 'number' && salary < 500) {
+    return new Teacher();
+  } else {
+    return new Director();
+  }
+}
+
+// Test createEmployee function
+const lowSalaryEmployee = createEmployee(300); // Should return a Teacher instance
+const highSalaryEmployee = createEmployee(1000); // Should return a Director instance
+
+console.log(lowSalaryEmployee.workFromHome());
+console.log(lowSalaryEmployee.getCoffeeBreak());
+console.log(lowSalaryEmployee.workTeacherTasks());
+
+console.log(highSalaryEmployee.workFromHome());
+console.log(highSalaryEmployee.getCoffeeBreak());
+console.log(highSalaryEmployee.workDirectorTasks());
